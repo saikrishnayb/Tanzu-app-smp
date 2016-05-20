966685371bd07d2f0f7830c7d11a1c79b9b9e7a5
Mudassir Mukhtarahmed <mudassir.mukhtarahmed@igate.com>
2016-05-20 20:33:14 +0000

    Changes for home screen to load
    git-svn-id: https://svn.penske.com/svn/ecomm/webprojects/suppliermgmt/branches/qa/phase2.2/suppliermgmt@54208 1dbbddac-570e-0410-aece-e783461d2649

--- targeted/qa/NavigationController.java
+++ targeted/qa/NavigationController.java
@@ -24,7 +24,7 @@ import com.penske.apps.suppliermgmt.model.UserContext;
 * @Contact        :
 * @Description    : Class for redirecting to different application
 * @History        :
-* This class is under targeted folder, make sure the changes to be merged in targeted folder also
+*This class is under targeted folder, make sure the changes to be merged in targeted folder also
 ************************************************************************************/
 @Controller
 @RequestMapping("/navigation")
@@ -44,10 +44,12 @@ public class NavigationController extends BaseController {
 	
 	@ResponseBody
 	@RequestMapping(value = "/navigate", method = RequestMethod.POST)
-	public String navigateapplication(@RequestParam(value="path") String destination, 
+
+
+	public String navigateapplication(@RequestParam(value="path") String destination,
 			@RequestParam(value="controllerName") String controllerName,
 			@RequestParam(value="templateId") String templateId,
-									 HttpServletRequest request) throws Exception{
+			HttpServletRequest request) throws Exception{
 		
 		
 		String app = "";
@@ -61,13 +63,11 @@ public class NavigationController extends BaseController {
 			app = destination.toLowerCase();
 			 userContext = getUserContext(request);
 			if("adminconsole".equalsIgnoreCase(destination)){
-				url.append(request.getContextPath()).append(ApplicationConstants.SLASH).append(ApplicationConstants.PENSKE_USER_URL);
+				url.append(request.getContextPath()).append(ApplicationConstants.SLASH).append("admin-console/app-config/dynamic-rules.htm");
 			}else if("Home".equalsIgnoreCase(destination)){
 				url.append(request.getContextPath()).append(ApplicationConstants.SLASH).append("home/homePage.htm");
 			}else{
-
-			
-				url.append(ApplicationConstants.SLASH).append(app).append(ApplicationConstants.ENTRY_SERVLET);
+				url.append(ApplicationConstants.SLASH).append(app).append(ApplicationConstants.ENTRY_SERVLET).append(userContext.getUserSSO());
 
 				if(controllerName!=null){
 					url.append(ApplicationConstants.CONTROLLER_NAME).append(controllerName);
