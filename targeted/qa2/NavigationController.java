3b63fd6799b2912bd26911c60f285f97ea739013
David Roth <david.roth@penske.com>
2016-11-14 19:54:40 +0000

    Fix missing mappings in targeted directory for help file integration.
    git-svn-id: https://svn.penske.com/svn/ecomm/webprojects/suppliermgmt/branches/qa/phase2.2/suppliermgmt@58417 1dbbddac-570e-0410-aece-e783461d2649

--- targeted/qa2/NavigationController.java
+++ targeted/qa2/NavigationController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.penske.apps.suppliermgmt.common.constants.ApplicationConstants;
 import com.penske.apps.suppliermgmt.model.UserContext;
+import com.penske.apps.suppliermgmt.service.HelpService;
 
 
 /************************************************************************************
