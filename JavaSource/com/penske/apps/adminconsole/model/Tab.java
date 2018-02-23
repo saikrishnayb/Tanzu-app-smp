9aa8f72a35276fa799fbeb1e0ca12c1ca55d6a0c
Erik Munoz <erik.munoz@penske.com>
2018-02-23 21:01:26 +0000

    Remove accordions that have no permissions
    git-svn-id: https://svn.penske.com/svn/ecomm/webprojects/suppliermgmt/suppliermgmt/branches/dev@64829 1dbbddac-570e-0410-aece-e783461d2649

--- JavaSource/com/penske/apps/adminconsole/model/Tab.java
+++ JavaSource/com/penske/apps/adminconsole/model/Tab.java
@@ -6,77 +6,81 @@ import java.util.List;
  * @author 600143568
  */
 public class Tab {
-	private int tabId;						  // the tab ID
-	private String tabName;					  // the tab name
-	private String imageUrl;				  // the tab image URL
-	private List<AlertHeader> alertHeaders;	  // the list of alert headers for the tab
-	private List<Permission> permissions;
-	private int defaultTemplateId;
-	private String tabKey;
-	
-	// Getters
-	public int getTabId() {
-		return tabId;
-	}
-	
-	public String getTabName() {
-		return tabName;
-	}
+    private int tabId;						  // the tab ID
+    private String tabName;					  // the tab name
+    private String imageUrl;				  // the tab image URL
+    private List<AlertHeader> alertHeaders;	  // the list of alert headers for the tab
+    private List<Permission> permissions;
+    private int defaultTemplateId;
+    private String tabKey;
 
-	public String getImageUrl() {
-		return imageUrl;
-	}
+    // Getters
+    public int getTabId() {
+        return tabId;
+    }
 
-	public List<AlertHeader> getAlertHeaders() {
-		return alertHeaders;
-	}
-	
-	public List<Permission> getPermissions() {
-		return permissions;
-	}
-	
-	public int getDefaultTemplateId() {
-		return this.defaultTemplateId;
-	}
+    public String getTabName() {
+        return tabName;
+    }
 
-	// Setters
-	public void setTabId(int tabId) {
-		this.tabId = tabId;
-	}
-	
-	public void setTabName(String tabName) {
-		this.tabName = tabName;
-	}
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
+    public List<AlertHeader> getAlertHeaders() {
+        return alertHeaders;
+    }
 
-	public void setAlertHeaders(List<AlertHeader> alertHeaders) {
-		this.alertHeaders = alertHeaders;
-	}
-	
-	public void setPermissions(List<Permission> permissions) {
-		this.permissions = permissions;
-	}
-	
-	public void setDefaultTemplateId(int defaultTemplateId) {
-		this.defaultTemplateId = defaultTemplateId;
-	}
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
 
-	// Overridden Methods
-	@Override
-	public String toString() {
-		return "Tab [tabId=" + tabId + ", tabName=" + tabName
-				+ ", imageUrl=" + imageUrl + ", alertHeaders=" + alertHeaders
-				+ "]";
-	}
+    public int getDefaultTemplateId() {
+        return this.defaultTemplateId;
+    }
 
-	public String getTabKey() {
-		return tabKey;
-	}
+    public int getPermissionsCount() {
+        return permissions == null? 0 : permissions.size();
+    }
 
-	public void setTabKey(String tabKey) {
-		this.tabKey = tabKey;
-	}
+    // Setters
+    public void setTabId(int tabId) {
+        this.tabId = tabId;
+    }
+
+    public void setTabName(String tabName) {
+        this.tabName = tabName;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setAlertHeaders(List<AlertHeader> alertHeaders) {
+        this.alertHeaders = alertHeaders;
+    }
+
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public void setDefaultTemplateId(int defaultTemplateId) {
+        this.defaultTemplateId = defaultTemplateId;
+    }
+
+    // Overridden Methods
+    @Override
+    public String toString() {
+        return "Tab [tabId=" + tabId + ", tabName=" + tabName
+                + ", imageUrl=" + imageUrl + ", alertHeaders=" + alertHeaders
+                + "]";
+    }
+
+    public String getTabKey() {
+        return tabKey;
+    }
+
+    public void setTabKey(String tabKey) {
+        this.tabKey = tabKey;
+    }
 }
