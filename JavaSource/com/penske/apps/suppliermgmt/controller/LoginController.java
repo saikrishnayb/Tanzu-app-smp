03dd3c270815e1729a5d1acdb3fe2cdbd0d8b2e8
John Shiffler <john.shiffler@penske.com>
2019-01-25 19:58:27 +0000

    VOD-1731: Fix an exception that occurs when the session refresh URL is called by the front-end.
    git-svn-id: https://svn.penske.com/svn/ecomm/webprojects/suppliermgmt/suppliermgmt/branches/dev@69148 1dbbddac-570e-0410-aece-e783461d2649

--- JavaSource/com/penske/apps/suppliermgmt/controller/LoginController.java
+++ JavaSource/com/penske/apps/suppliermgmt/controller/LoginController.java
@@ -27,6 +27,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.penske.apps.suppliermgmt.annotation.VendorAllowed;
@@ -176,10 +177,10 @@ public class LoginController extends BaseController {
 
     @VendorAllowed
     @RequestMapping(value = "/validateSession", method = {RequestMethod.GET, RequestMethod.POST })
+    @ResponseBody
     protected void validateSession()
     {
         LOGGER.info("Inside suppliermgmt validateSession() ");
-
     }
 
 
