688adefee79e804b41cc62129db0c57f1d9df8b3
Erik Munoz <erik.munoz@penske.com>
2019-02-26 20:52:00 +0000

    VOD-1817: Prevent the iframe resizing from happening at machine gun speed
    git-svn-id: https://svn.penske.com/svn/ecomm/webprojects/suppliermgmt/suppliermgmt/branches/dev@69436 1dbbddac-570e-0410-aece-e783461d2649

--- WebContent/js/global/iframe-resizer.js
+++ WebContent/js/global/iframe-resizer.js
@@ -1,8 +1,12 @@
 var iframeResizer = (function() {
   
+  //Polyfill for <= ie11
+  if (Number.parseInt === undefined) Number.parseInt = window.parseInt;
+  
   let _iframeNode = window.parent.document.querySelector('#mainFrame');
   let _iframeWindow = window.parent.window.frames[0];
   let _resizeIframeTimeoutID = null;
+  let _problemClassRegex = /.ui-state-hover/gm
   let _observer = null;
   
   let _getElementHeight = function getElementHeight(element) {
@@ -53,10 +57,34 @@ var iframeResizer = (function() {
     if(_observer !== null) throw 'ResizeListener already initialized'; 
     
     let targetNode = _iframeWindow.document.body;
-    let config = {attributes: true, characterData: true,childList: true, subtree: true};
+    let config = {
+      attributeFilter : ["class"],
+      attributeOldValue : true,
+      attributes : true,
+      characterData : true,
+      childList : true,
+      subtree : true
+    };
     
     _observer = new MutationObserver(function(mutationsList, observer) {
-      resizeIframe();
+      
+      let isAllProblemChildClasses = true; //Guilty till proven innocent
+      
+      mutationsList.every(function(mutationRecord) {
+        
+        if(mutationRecord.attributeName === 'class') {
+          let onlyUiStateHoverChange = mutationRecord.oldValue.replace(_problemClassRegex, '') ===
+                                       mutationRecord.target.classList.toString().replace(_problemClassRegex, '');
+          
+          if(!onlyUiStateHoverChange) isAllProblemChildClasses = false;
+        } else {
+          isAllProblemChildClasses = false;
+        }
+        
+      });
+      
+      if(!isAllProblemChildClasses) resizeIframe();
+      
     });
     
     _observer.observe(targetNode, config);
