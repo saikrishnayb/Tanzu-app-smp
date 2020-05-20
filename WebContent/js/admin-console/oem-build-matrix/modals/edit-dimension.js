9fd0474da6c8b704da3bbcf5fde2863fcc5e4ab6
Karthika Gajendran <karthika.gajendran@penske.com>
2020-05-20 10:54:24 +0000

    VOD-2505  OEM Build Matrix - Body Plant Exceptions - Save button enable/disable
    git-svn-id: https://svn.penske.com/svn/ecomm/webprojects/suppliermgmt/suppliermgmt/branches/dev@76157 1dbbddac-570e-0410-aece-e783461d2649

--- WebContent/js/admin-console/oem-build-matrix/modals/edit-dimension.js
+++ WebContent/js/admin-console/oem-build-matrix/modals/edit-dimension.js
@@ -1,15 +1,32 @@
 $('.attibute-values-checkbox').on("change", function() {
-
+	var capabilityUpdatelist = [];
+	$('input[type=checkbox]:not(:checked)').each(function() {
+		capabilityUpdatelist.push($(this).val());
+	});
 	if ($(this).is(':checked')) {
 		$("#update-capability").removeClass("buttonDisabled");
 	} else {
 		$("#update-capability").addClass("buttonDisabled");
 	}
+	if (capabilityUpdatelist.length == 0) {
+		$("#update-capability").addClass("buttonDisabled");
+	} else {
+		$("#update-capability").removeClass("buttonDisabled");
+	}
+
 });
 
 $('.disallow-attibute-values-checkbox').on("change", function() {
-
+	var capabilityUpdateslist = [];
+	$('input[type=checkbox]:not(:checked)').each(function() {
+		capabilityUpdateslist.push($(this).val());
+	});
 	if ($(this).is(':checked')) {
+		$("#update-capability").addClass("buttonDisabled");
+	} else {
+		$("#update-capability").removeClass("buttonDisabled");
+	}
+	if (capabilityUpdateslist.length == 0) {
 		$("#update-capability").addClass("buttonDisabled");
 	} else {
 		$("#update-capability").removeClass("buttonDisabled");
