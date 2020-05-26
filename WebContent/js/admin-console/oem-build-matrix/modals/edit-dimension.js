a07b1b3c34d9f1f651a271808ff9d92b5060d728
Karthika Gajendran <karthika.gajendran@penske.com>
2020-05-26 10:04:05 +0000

    VOD-2505  OEM Build Matrix -Body Plant Exceptions - Edit Dimension
    git-svn-id: https://svn.penske.com/svn/ecomm/webprojects/suppliermgmt/suppliermgmt/branches/dev@76177 1dbbddac-570e-0410-aece-e783461d2649

--- WebContent/js/admin-console/oem-build-matrix/modals/edit-dimension.js
+++ WebContent/js/admin-console/oem-build-matrix/modals/edit-dimension.js
@@ -1,14 +1,15 @@
-var selectedAttribtelist = [];
+var selectedAttributelist = [];
 $('#attribute-values-div input:checked').each(function() {
-	selectedAttribtelist.push($(this).val());
+	selectedAttributelist.push($(this).val());
 });
-var nonSelectedAttribtelist = [];
+
+var nonselectedAttributelist = [];
 $('input[type=checkbox]:not(:checked)').each(function() {
-	nonSelectedAttribtelist.push($(this).val());
+	nonselectedAttributelist.push($(this).val());
 });
 
 $('.attibute-values-checkbox').on("change", function() {
-	if (selectedAttribtelist.length > 0 || nonSelectedAttribtelist.length > 0) {
+	if (selectedAttributelist.length > 0 || nonselectedAttributelist.length > 0) {
 		$("#update-capability").removeClass("buttonDisabled");
 	} else if ($(this).is(':checked')) {
 		$("#update-capability").removeClass("buttonDisabled");
@@ -18,7 +19,7 @@ $('.attibute-values-checkbox').on("change", function() {
 });
 
 $('.disallow-attibute-values-checkbox').on("change", function() {
-	if (nonSelectedAttribtelist.length > 0) {
+	if (nonselectedAttributelist.length > 0) {
 		$("#update-capability").removeClass("buttonDisabled");
 	} else if ($(this).is(':checked')) {
 		$("#update-capability").removeClass("buttonDisabled");
