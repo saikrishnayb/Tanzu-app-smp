c664999b36a76d945d5fd60d976c68e5125211ac
John Shiffler <john.shiffler@penske.com>
2021-02-05 13:24:40 +0000

    VOD-3392: Added new controllers for App Config tab using v2 page template.
    git-svn-id: https://svn.penske.com/svn/ecomm/webprojects/suppliermgmt/suppliermgmt/branches/dev@79749 1dbbddac-570e-0410-aece-e783461d2649

---
+++ JavaSource/com/penske/apps/adminconsole/controller/AppConfigV2Controller.java
@@ -0,0 +1,15 @@
+/**
+ * @author john.shiffler (600139252)
+ */
+package com.penske.apps.adminconsole.controller;
+
+import com.penske.apps.suppliermgmt.annotation.DefaultController;
+
+/**
+ * Controller to render pages on the App Config sub-tab that use the v2 page template.
+ */
+@DefaultController
+public class AppConfigV2Controller
+{
+
+}
---
+++ JavaSource/com/penske/apps/adminconsole/controller/AppConfigV2RestController.java
@@ -0,0 +1,15 @@
+/**
+ * @author john.shiffler (600139252)
+ */
+package com.penske.apps.adminconsole.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * A controller to answer AJAX requests on the App Config subtab, for pages that use the v2 page template.
+ */
+@RestController
+public class AppConfigV2RestController
+{
+	
+}
