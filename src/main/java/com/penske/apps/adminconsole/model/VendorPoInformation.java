d7234069438c46da7d38731a746f736b76674696
Justin Frey <justin.frey@penske.com>
2022-03-10 10:35:14 -0500

    VOD-3366: Admin Console - Vendor Users - Export List of Vendor Users changing VendorPoInformation to use vendor IDs

--- src/main/java/com/penske/apps/adminconsole/model/VendorPoInformation.java
+++ src/main/java/com/penske/apps/adminconsole/model/VendorPoInformation.java
@@ -3,24 +3,26 @@ package com.penske.apps.adminconsole.model;
 import java.time.LocalDate;
 
 public class VendorPoInformation {
-	private int vendorNumber;
+	private int vendorId;
 	private LocalDate lastPoDate;
 	private int posIssuedInLast3Years;
 	
 	protected VendorPoInformation() {}
 	
-	public VendorPoInformation(int vendorNumber) {
-		this.vendorNumber = vendorNumber;
+	public VendorPoInformation(int vendorId) {
+		this.vendorId = vendorId;
 		this.lastPoDate = null;
 		this.posIssuedInLast3Years = 0;
 	}
-	
-	public int getVendorNumber() {
-		return vendorNumber;
+
+	public int getVendorId() {
+		return vendorId;
 	}
+	
 	public LocalDate getLastPoDate() {
 		return lastPoDate;
 	}
+	
 	public int getPosIssuedInLast3Years() {
 		return posIssuedInLast3Years;
 	}
